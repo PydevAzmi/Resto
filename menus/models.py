ad7f25c2cd4f0efcf1acc9e2524f4cb977d2dedd
pydevazmi <pydevazmi@gmail.com>
2023-10-09 22:23:46 +0300

    Working on menus app models

--- menus/models.py
+++ menus/models.py
@@ -1,3 +1,55 @@
 from django.db import models
 
 # Create your models here.
+
+COMPONENT_CHOICES = [
+    ("Regular" , "Regular"),
+    ("Extra" , "Extra"),
+    ("Spicy" , "Spicy"),
+    ("1L", "1L"),
+    ("2L", "2L"),
+    ("Can", "Can"),
+]
+
+class Category(models.Model):
+    name = models.CharField(max_length=50)
+    image = models.ImageField(upload_to=None)
+
+    def __str__(self):
+        return f"{self.name}"
+
+class ComponentChoises(models.Model):
+    name = models.CharField(max_length=100)
+    type = models.CharField(max_length=20 , choices = COMPONENT_CHOICES)
+    price = models.DecimalField(max_digits=5, decimal_places=2)
+    ingredient = models.ForeignKey("Ingredient", on_delete=models.CASCADE)
+
+    def __str__(self):
+        return f"{self.name} - {self.type} - {self.price}"
+
+class Ingredient(models.Model):
+    name = models.CharField(max_length=50)
+
+    def __str__(self):
+        return f"{self.name}"
+
+class MenuItemIngredient(models.Model):
+    ingredient = models.ForeignKey(Ingredient, on_delete=models.CASCADE)
+    menu_item = models.ForeignKey('MenuItem', on_delete=models.CASCADE)
+    quantity = models.PositiveIntegerField(null=True, blank=True)
+
+    def __str__(self):
+        return f"{self.menu_item} - {self.quantity} {self.ingredient}"
+
+class MenuItem(models.Model):
+    name = models.CharField(max_length=50)
+    description = models.TextField()
+    image = models.ImageField(upload_to=None)
+    category = models.ForeignKey(Category, on_delete=models.CASCADE)
+    ingredients = models.ManyToManyField(Ingredient, through=MenuItemIngredient)
+    price = models.DecimalField(max_digits=5, decimal_places=2)
+    is_sale = models.BooleanField(default=False)
+    sale_price = models.DecimalField(max_digits=5, decimal_places=2)
+
+    def __str__(self):
+        return f"{self.name}"
