03887c617daadabf225aca70f3744d061a8c9e37
pydevazmi <pydevazmi@gmail.com>
2023-10-14 04:10:58 +0300

    Adding CORS headers to project settings

--- project/settings.py
+++ project/settings.py
@@ -47,11 +47,15 @@ INSTALLED_APPS = [
     # third-party apps
     'rest_framework',
     'django_filters',
+    "corsheaders",
 ]
 
 MIDDLEWARE = [
     "django.middleware.security.SecurityMiddleware",
     "django.contrib.sessions.middleware.SessionMiddleware",
+
+    "corsheaders.middleware.CorsMiddleware",
+
     "django.middleware.common.CommonMiddleware",
     "django.middleware.csrf.CsrfViewMiddleware",
     "django.contrib.auth.middleware.AuthenticationMiddleware",
@@ -148,3 +152,5 @@ REST_FRAMEWORK = {
         'rest_framework.permissions.IsAuthenticatedOrReadOnly',
     ]
 }
+
+CORS_ALLOW_ALL_ORIGINS = True
