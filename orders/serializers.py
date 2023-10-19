acb71aabab504451e1fae0c417ad5fd2095e9de1
pydevazmi <pydevazmi@gmail.com>
2023-10-20 01:26:06 +0300

    Working on CustomizationUpdateSerializer and Validate Component

--- orders/serializers.py
+++ orders/serializers.py
@@ -1,11 +1,14 @@
 from rest_framework import serializers
 from .models import Cart, CartItemDetail, Order, OrderItemDetail, Customization, SpecialInstructions
-from menus.models import MenuItem, MenuItemIngredient
+from menus.models import MenuItem, MenuItemIngredient, ComponentChoises
 from menus.serializers import ComponentChoisesSerializer
 from django.shortcuts import get_object_or_404
+
+
 class CustomizationSerializer(serializers.ModelSerializer):
     component = ComponentChoisesSerializer(read_only = True)
     ingredient = serializers.SerializerMethodField(read_only = True)
+    
     class Meta:
         model = Customization
         fields = (
@@ -16,10 +19,34 @@ class CustomizationSerializer(serializers.ModelSerializer):
     def get_ingredient(self, obj):
         return obj.ingredient.name
     
+class CustomizationUpdateSerializer(serializers.ModelSerializer):
+    ingredient = serializers.SerializerMethodField(read_only = True)
+    queryset = ComponentChoises.objects.all()
+    component = serializers.PrimaryKeyRelatedField(queryset= queryset)
+
+    class Meta:
+        model = Customization
+        fields = (
+            'id',
+            'ingredient',
+            "component",
+        )
+
+    def get_ingredient(self, obj):
+        return obj.ingredient.name
+    
+    def validate_component(self, value):
+        customization_instance = self.context.get("customization_instance")
+        allowed_components = ComponentChoises.objects.filter(ingredient=customization_instance.ingredient)
+        if value not in allowed_components:
+            raise serializers.ValidationError("Invalid component choice for the ingredient.")
+        return value
+    
+
 class MenuItemIngredientSerializer(serializers.ModelSerializer):
     name = serializers.SerializerMethodField()
     is_optional = serializers.SerializerMethodField()
-    #components_choises = ComponentChoisesSerializer(many = True, source = 'ingredient.componentchoises_set')
+
     class Meta:
         model = MenuItemIngredient
         fields = (
@@ -34,8 +61,10 @@ class MenuItemIngredientSerializer(serializers.ModelSerializer):
     def get_is_optional(self, obj):
         return obj.ingredient.is_optional
 
+
 class ItemSerializer(serializers.ModelSerializer):
     Ingredients = MenuItemIngredientSerializer(many = True, source = 'menuitemingredient_set', read_only = True)
+
     class Meta:
         model = MenuItem
         fields = (
@@ -43,16 +72,37 @@ class ItemSerializer(serializers.ModelSerializer):
             "Ingredients",
             )
 
-class CartItemDetailSerailizer(serializers.ModelSerializer):
+
+class CartItemSerailizer(serializers.ModelSerializer):
     item = ItemSerializer()
+
     class Meta:
         model = CartItemDetail
         fields = (
+            "id",
             "item",
             "quantity",
             "price",
             "details",
             )
+
+
+class CartItemDetailSerailizer(serializers.ModelSerializer):
+    item = serializers.SerializerMethodField()
+
+    class Meta:
+        model = CartItemDetail
+        fields = (
+            "id",
+            "item",
+            "quantity",
+            "price",
+            "details",
+            )
+        
+    def get_item(self, obj):
+        return obj.item.name
+
 class SpecialInstructionsSerializer(serializers.ModelSerializer):
     class Meta:
         model = SpecialInstructions
@@ -63,9 +113,11 @@ class SpecialInstructionsSerializer(serializers.ModelSerializer):
             'ketchup',
             'cutlery',)
         
+
 class CartSerailizer(serializers.ModelSerializer):
-    cart_items = CartItemDetailSerailizer(many = True, source="cart", read_only = True) 
+    cart_items = CartItemSerailizer(many = True, source="cart", read_only = True) 
     cart_instructions = SpecialInstructionsSerializer(many = True, read_only = True)
+
     class Meta:
         model = Cart
         fields = (
@@ -78,12 +130,14 @@ class CartSerailizer(serializers.ModelSerializer):
             'cart_instructions'
             )
         
+
 class CartUpdateSerailizer(serializers.ModelSerializer):
     queryset = MenuItem.objects.all()
-    cart_items = CartItemDetailSerailizer(many = True, source="cart", read_only = True)
+    cart_items = CartItemSerailizer(many = True, source="cart", read_only = True)
     item = serializers.ChoiceField(choices=queryset, write_only = True)
     quantity = serializers.IntegerField(write_only = True, default= 1)
     cart_instructions = SpecialInstructionsSerializer(many = True, read_only = True)
+
     class Meta:
         model = Cart
         fields = (
@@ -103,7 +157,8 @@ class CartUpdateSerailizer(serializers.ModelSerializer):
                 quantity = 1 if 'quantity' not in validated_data else validated_data['quantity']
                 CartItemDetail.objects.create(cart=instance, item=item, quantity=quantity)
         return super().update(instance, validated_data)
-        
+
+
 class OrderItemDetailSerailizer(serializers.ModelSerializer):
     class Meta:
         model = OrderItemDetail
@@ -113,6 +168,7 @@ class OrderItemDetailSerailizer(serializers.ModelSerializer):
             "price",
             "quantity",
             )
+
 
 class OrderSerailizer(serializers.ModelSerializer):
     class Meta:
