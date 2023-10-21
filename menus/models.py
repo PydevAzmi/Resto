8b8a2d8c99fd783c3eb21bd3b05ad1127cfbfe4d
pydevazmi <pydevazmi@gmail.com>
2023-10-22 00:01:07 +0300

    Reformat Models in menu app

--- menus/models.py
+++ menus/models.py
@@ -1,22 +1,26 @@
 from django.db import models
-from django.contrib.auth.models import User
+from project.settings import AUTH_USER_MODEL
 from django.core.validators import MaxValueValidator, MinValueValidator
+
 # Create your models here.
 
 COMPONENT_CHOICES = [
-    ("Regular" , "Regular"),
-    ("Extra" , "Extra"),
-    ("Spicy" , "Spicy"),
+    ("Regular", "Regular"),
+    ("Extra", "Extra"),
+    ("Spicy", "Spicy"),
     ("1L", "1L"),
     ("2L", "2L"),
     ("Can", "Can"),
 ]
 
+
 def item_image_path(instance, file_name):
     return f"images/{instance.category}/{instance.name}/{file_name}"
 
+
 def ingredient_image_path(instance, file_name):
     return f"images/ingredient/{instance.ingredient}/{instance.type}/{file_name}"
+
 
 class Category(models.Model):
     name = models.CharField(max_length=50)
@@ -25,9 +29,10 @@ class Category(models.Model):
     def __str__(self):
         return f"{self.name}"
 
+
 class ComponentChoises(models.Model):
     name = models.CharField(max_length=100)
-    type = models.CharField(max_length=20 , choices = COMPONENT_CHOICES)
+    type = models.CharField(max_length=20, choices=COMPONENT_CHOICES)
     price = models.DecimalField(max_digits=5, decimal_places=2)
     image = models.ImageField(upload_to=ingredient_image_path, null=True, blank=True)
     ingredient = models.ForeignKey("Ingredient", on_delete=models.CASCADE)
@@ -35,11 +40,14 @@ class ComponentChoises(models.Model):
     def __str__(self):
         return f"{self.name} - {self.type} - {self.price}"
 
+
 class Ingredient(models.Model):
     name = models.CharField(max_length=50)
     is_optional = models.BooleanField(default=False)
+
     def __str__(self):
         return f"{self.name}"
+
 
 class MenuItemIngredient(models.Model):
     ingredient = models.ForeignKey(Ingredient, on_delete=models.CASCADE)
@@ -48,6 +56,7 @@ class MenuItemIngredient(models.Model):
 
     def __str__(self):
         return f"{self.menu_item} - {self.quantity} {self.ingredient}"
+
 
 class MenuItem(models.Model):
     name = models.CharField(max_length=50)
@@ -58,9 +67,9 @@ class MenuItem(models.Model):
     price = models.DecimalField(max_digits=5, decimal_places=2)
     is_sale = models.BooleanField(default=False)
     is_fav = models.BooleanField(default=False)
-    sale = models.FloatField(default=0, validators = [MaxValueValidator(1), MinValueValidator(0)], null=True,blank=True)
-    
-    @property   
+    sale = models.FloatField(default=0, validators=[MaxValueValidator(1), MinValueValidator(0)], null=True, blank=True)
+
+    @property
     def sale_price(self):
         if self.is_sale:
             return self.price - round(float(self.price) * (self.sale))
@@ -73,8 +82,7 @@ class MenuItem(models.Model):
 
 class Favourites(models.Model):
     item = models.ForeignKey(MenuItem, related_name="fav_item", on_delete=models.CASCADE)
-    user = models.ForeignKey(User, on_delete=models.CASCADE)
-    
+    user = models.ForeignKey(AUTH_USER_MODEL, on_delete=models.CASCADE)
+
     def __str__(self):
         return f"{self.user} Likes {self.item}"
-    
