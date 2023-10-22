a6a9018d4071665ff062da188bbac5121a336fb1
pydevazmi <pydevazmi@gmail.com>
2023-10-23 00:04:14 +0300

    Install Djoser and DRF Simple JWT

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,6 +1,7 @@
-from django.urls import path
+from django.urls import path, include
 app_name = "accounts"
 
 urlpatterns = [
-    
+    path('', include('djoser.urls')),
+    path('', include('djoser.urls.jwt')),
 ]
--- project/settings.py
+++ project/settings.py
@@ -49,6 +49,7 @@ INSTALLED_APPS = [
 
     # third-party apps
     'rest_framework',
+    'djoser',
     'django_filters',
     "corsheaders",
 ]
@@ -145,17 +146,46 @@ MEDIA_ROOT  = BASE_DIR / 'media'
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 
+SIMPLE_JWT = {
+   'AUTH_HEADER_TYPES': ('JWT',),
+}
 
 REST_FRAMEWORK = {
     
     "DEFAULT_AUTHENTICATION_CLASSES" : [
         'rest_framework.authentication.SessionAuthentication',
-        'rest_framework.authentication.BasicAuthentication'
+        'rest_framework_simplejwt.authentication.JWTAuthentication',
     ],
 
-    'DEFAULT_PERMISSION_CLASSES': [
-        'rest_framework.permissions.IsAuthenticatedOrReadOnly',
-    ]
 }
+
+DJOSER = {
+    'LOGIN_FIELD': 'email',
+    'USER_CREATE_PASSWORD_RETYPE': True,
+    'USERNAME_CHANGED_EMAIL_CONFIRMATION': True,
+    'PASSWORD_CHANGED_EMAIL_CONFIRMATION': True,
+    'SET_USERNAME_RETYPE': True,
+    'SET_PASSWORD_RETYPE': True, 
+    'PASSWORD_RESET_CONFIRM_URL': 'password/reset/confirm/{uid}/{token}',
+    'USERNAME_RESET_CONFIRM_URL': 'email/reset/confirm/{uid}/{token}',
+    'ACTIVATION_URL': 'activate/{uid}/{token}',
+    'SEND_ACTIVATION_EMAIL': True,
+    'SEND_CONFIRMATION_EMAIL': True,
+    'SERIALIZERS': {
+        'user': 'accounts.serializers.UserCreateSerializer',
+        'user_create': 'accounts.serializers.UserCreateSerializer',
+        'user_delete': 'djoser.serializers.UserDeleteSerializer',
+    },
+}
+
+# E-mail Settings
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_HOST_USER = 'pydevazmi@gmail.com'
+EMAIL_HOST_PASSWORD = "EMAIL_HOST_PASSWORD"
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
+EMAIL_USE_SSL = False
+
 
 CORS_ALLOW_ALL_ORIGINS = True
