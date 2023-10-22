0ff358b84e0dbd9aa57f959f2edead7eb527a81f
pydevazmi <pydevazmi@gmail.com>
2023-10-23 00:08:04 +0300

    Testing API Endpoints

--- client/cart_list.py
+++ client/cart_list.py
@@ -1,6 +1,11 @@
 import requests
 
-api_url = "http://127.0.0.1:8000/api/orders/cart"
+url = "http://127.0.0.1:8000/api/orders/cart"
 
-response = requests.get(api_url)
-print(response.json())
+headers = {
+  'Authorization': 'JWT eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ0b2tlbl90eXBlIjoiYWNjZXNzIiwiZXhwIjoxNjk4MDA3NjE2LCJpYXQiOjE2OTgwMDczMTYsImp0aSI6ImNmMjY4M2Q0ZmNmMDQ1OWJiYWY3MmM5NzRjOTg3YmQ1IiwidXNlcl9pZCI6MX0.rROyjWzPgoKEhiftyHkV-QfVbdbHxK5y1zItGnaP-VQ'
+}
+
+response = requests.request("GET", url, headers=headers)
+
+print(response.text)
