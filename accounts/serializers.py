299a64a7b32033c9c99b1d665bc842fea2e4b023
pydevazmi <pydevazmi@gmail.com>
2023-10-26 02:12:14 +0300

    sends Django's user_logged_in signal

--- accounts/serializers.py
+++ accounts/serializers.py
@@ -1,5 +1,7 @@
 from django.contrib.auth import get_user_model
 from djoser.serializers import UserCreateSerializer
+from rest_framework_simplejwt.serializers import TokenObtainPairSerializer
+from django.contrib.auth.signals import user_logged_in
 
 User = get_user_model()
 
@@ -13,3 +15,27 @@ class UserCreateSerializer(UserCreateSerializer):
             'username',
             'password',
             )
+
+
+class MyTokenObtainPairSerializer(TokenObtainPairSerializer):
+    @classmethod
+    def get_token(cls, user):
+        token = super().get_token(user)
+        # Add custom claims
+        token['username'] = user.username
+        token['is_super_user'] = user.is_superuser
+        token['email'] = user.email
+
+        return token
+    
+    def validate(self, attrs):
+        data = super().validate(attrs)
+
+        refresh = self.get_token(self.user)
+
+        data['refresh'] = str(refresh)
+        data['access'] = str(refresh.access_token)
+
+        # sends Django's user_logged_in signal
+        user_logged_in.send(sender=self.user.__class__, request=self.context['request'], user=self.user)
+        return data
